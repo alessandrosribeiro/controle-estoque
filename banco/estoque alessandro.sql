febc262b6107f1eba5b5be11d91c68af1c4ba6e2
alessandrosribeiro <57419425+alessandrosribeiro@users.noreply.github.com>
2022-12-08 20:49:44 -0300

    Add files via upload

--- banco/estoque alessandro.sql
+++ banco/estoque alessandro.sql
@@ -7,56 +7,14 @@
  create database dbestoque;
  use dbestoque;
  
- create table usuario (
+ create table usuarios (
 	id int primary key auto_increment,
     usuario varchar(50) not null,
     login varchar(20) not null,
     senha varchar(250) not null
 );
 
-describe usuario;
-
--- Criar Tabela
-show tables;
-
-
--- Cruid Creat (insert)
-insert into usuario (usuario,login,senha) 
-values ('sandro','sandro01','123@sandro');
-
-insert into usuario (usuario,login,senha) 
-values ('felipe','felipe01','123@felipe');
-
-insert into usuario (usuario,login,senha) 
-values ('ricardo','ricard01','123@ricardo');
-
-insert into usuario (usuario,login,senha) 
-values ('jose','jose01','123@jose');
-
-insert into usuario (usuario,login,senha) 
-values ('milena','milena01','123@milena');
-
-
--- a linha abaixo cria apelidos aos campos (cabeçalho) das tabelas - alterar nome na tabela
-select id as Código, usuario as Nome, login as Login, senha as Senha from usuario;
-
-
-
--- cruid Update (importante usar a clausula where associada ao id) - alterar usuario pelo id
-update usuario set usuario = 'Sandrosr' where id = 7;
-
-
--- cruid Delete (importante usar a clausula where associada ao id) - Deletar usuário pelo id
-delete from usuario where id = 11;
-
-
--- ________________________________________________________________________________________________________________________________
-
--- a linha abaixo seleciona todos os registros da tabela - Visualizar tabela
-select * from usuario;
-
--- a linha abaixo seleciona todos os registros da tabela - Visualizar usuario unico
-select * from usuario where id = 7;
+describe usuarios;
 
 /************** CRUD *****************/
 
@@ -71,4 +29,8 @@ select * from usuario where id = 7;
 -- DELETE (excluir um usuário específico)
 
 -- Gerar a documentação - Modelo ER (engenharia reversa)
+
+
+
+
  
